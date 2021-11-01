313cda5860ac3203bf1521a1d3aff48209813943
Morad Abu Alkeaan <moradk1234@gmail.com>
2021-11-01 17:01:17 +0200

    Add files via upload

---
+++ portfolio - template/js/main.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-btn i");
+
+let val;
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("sticky");
+        scrollBtn.style.display = "block";
+    } 
+    else {
+        nav.classList.remove("sticky");
+        scrollBtn.style.display = "none";
+    }
+};
+
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.onclick = function () {
+    navBar.classList.add("active");
+    menuBtn.style.opacity = "0";
+    menuBtn.style.pointerEvents = "none";
+    body.style.overflow = "hidden";
+    scrollBtn.style.pointerEvents = "none";
+};
+
+cancelBtn.onclick = function () {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+    scrollBtn.style.pointerEvents = "auto";
+};
+
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function () {
+        navBar.classList.remove("active");
+        menuBtn.style.opacity = "1";
+        menuBtn.style.pointerEvents = "auto";
+    });
+}
+
+
+let d = new Date();
+let expYears = document.getElementById("num");
+expYears.innerText = d.getFullYear() - 2000;
+
+let footerYear = document.getElementById("footer-year");
+footerYear.innerText = d.getFullYear();
